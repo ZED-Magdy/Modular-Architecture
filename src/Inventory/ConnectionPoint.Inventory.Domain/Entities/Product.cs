c3e2d62018f107e477d5388b535bfdcde86f1575
ZED-Magdy <zedmagdy@gmail.com>
2023-06-12 07:20:39 +0300

    Add ProductVariations Domain Models and migrations

--- src/Inventory/ConnectionPoint.Inventory.Domain/Entities/Product.cs
+++ src/Inventory/ConnectionPoint.Inventory.Domain/Entities/Product.cs
@@ -7,7 +7,35 @@ public class Product : ProductBase
     public string Barcode { get; set; } = string.Empty;
     public ProductType ProductType { get; set; } = ProductType.Single;
     public Guid? ParentProduct { get; set; }
-    public IList<Product> Variants { get; set; } = new List<Product>();
+    public IList<ProductVariation> Variations { get; set; } = new List<ProductVariation>();
     public IList<Category> Categories { get; set; } = new List<Category>();
     public IList<ProductUnitQuantity> ProductUnitQuantities { get; set; } = new List<ProductUnitQuantity>();
+    
+    public Product CreateVariations(List<Dictionary<Guid, Guid>> variationAttributes)
+    {
+        Variations = new List<ProductVariation>();
+
+        foreach (var attributes in variationAttributes)
+        {
+            var variation = new ProductVariation
+            {
+                ProductId = Id,
+                Variations = new List<Variation>()
+            };
+
+            foreach (var attribute in attributes)
+            {
+                variation.Variations.Add(new Variation
+                {
+                    ProductVariation = variation,
+                    AttributeId = attribute.Key,
+                    ValueId = attribute.Value
+                });
+            }
+
+            Variations.Add(variation);
+        }
+
+        return this;
+    }
 }
--- src/Inventory/ConnectionPoint.Inventory.Infrastructure/Persistence/EFCore/InventoryDBContext.cs
+++ src/Inventory/ConnectionPoint.Inventory.Infrastructure/Persistence/EFCore/InventoryDBContext.cs
@@ -16,9 +16,11 @@ public class InventoryDbContext : ModuleDbContext, IInventoryDbContext
     public DbSet<Service>? Services { get; set; }
     public DbSet<Category>? Categories { get; set; }
     public DbSet<Unit>? Units { get; set; }
+    public DbSet<ProductUnitQuantity>? ProductUnitQuantities { get; set; }
+    public DbSet<ProductVariation>? ProductVariations { get; set; }
+    public DbSet<Variation>? Variations { get; set; }
+    public DbSet<ProductAttribute>? ProductAttributes { get; set; }
 
-
-    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -54,5 +56,37 @@ public class InventoryDbContext : ModuleDbContext, IInventoryDbContext
             .HasConversion(
                 v =>  v == null ? "" : string.Join(',', v),
                 v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(Guid.Parse).ToList());
+        
+        modelBuilder.Entity<ProductVariation>()
+            .HasKey(pv => pv.Id);
+
+        modelBuilder.Entity<ProductVariation>()
+            .HasOne(pv => pv.Product)
+            .WithMany(p => p.Variations)
+            .HasForeignKey(pv => pv.ProductId);
+
+        modelBuilder.Entity<Variation>()
+            .HasKey(v => v.Id);
+
+        modelBuilder.Entity<Variation>()
+            .HasOne(v => v.ProductVariation)
+            .WithMany(pv => pv.Variations)
+            .HasForeignKey(v => v.ProductVariationId);
+
+        modelBuilder.Entity<ProductAttribute>()
+            .HasKey(pa => pa.Id);
+
+        modelBuilder.Entity<ProductAttribute>()
+            .HasMany(pa => pa.Variations)
+            .WithOne(v => v.Attribute)
+            .HasForeignKey(v => v.AttributeId);
+
+        modelBuilder.Entity<ProductAttributeValue>()
+            .HasKey(pav => pav.Id);
+
+        modelBuilder.Entity<ProductAttributeValue>()
+            .HasMany(pav => pav.Variations)
+            .WithOne(v => v.Value)
+            .HasForeignKey(v => v.ValueId);
     }
 }
