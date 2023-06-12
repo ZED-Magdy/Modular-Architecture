00709fca0fe8f78b7b664d283cdcd22e90cbf8ee
ZED-Magdy <zedmagdy@gmail.com>
2023-06-13 01:28:10 +0300

    Add service crud

--- src/Inventory/ConnectionPoint.Inventory.Application/MapperConfig.cs
+++ src/Inventory/ConnectionPoint.Inventory.Application/MapperConfig.cs
@@ -2,6 +2,7 @@
 using ConnectionPoint.Inventory.Application.Dtos;
 using ConnectionPoint.Inventory.Application.Dtos.Category;
 using ConnectionPoint.Inventory.Application.Dtos.Product;
+using ConnectionPoint.Inventory.Application.Dtos.Service;
 using ConnectionPoint.Inventory.Domain.Entities;
 
 namespace ConnectionPoint.Inventory.Application;
@@ -25,6 +26,16 @@ public class MapperConfig : Profile
                 .ForMember(p => p.Categories, o => o.Ignore())
                 .ForMember(p => p.TaxesIds, o => o.Ignore());
         #endregion
+        
+        #region Service
+            CreateMap<Service, ServiceDto>();
+            CreateMap<CreateServiceDto, Service>()
+                .ForMember(p => p.Categories, o => o.Ignore())
+                .ForMember(p => p.TaxesIds, o => o.Ignore());
+            CreateMap<UpdateServiceDto, Service>()
+                .ForMember(p => p.Categories, o => o.Ignore())
+                .ForMember(p => p.TaxesIds, o => o.Ignore());
+            #endregion
         
     }
 }
