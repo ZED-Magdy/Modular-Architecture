488efd61a6f1b79d32acb460aef69c285fa0ba30
Mohamed Khalel <mohamedkhalel6@gmail.com>
2023-06-12 12:33:29 +0200

    Add Vouchers Module to src #2

---
+++ src/Voucher/ConnectionPoint.Voucher.Infrastructure/Persistence/EFCore/VoucherRepository.cs
@@ -0,0 +1,12 @@
+﻿using ConnectionPoint.Core.Domain.Entities;
+using ConnectionPoint.Core.Infrastructure.Persistence;
+
+namespace ConnectionPoint.Voucher.Infrastructure.Persistence.EFCore
+{
+    public class VoucherRepository<TEntity> : EFCoreRepository<TEntity> where TEntity : FullAuditedEntity
+    {
+        public VoucherRepository(VoucherDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
